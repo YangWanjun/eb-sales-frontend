283178a5362373f96b728ff6709a1eb2a72ae295
YangWanjun <ywjsailor@gmail.com>
2019-03-14 11:20:40 +0900

    react redux

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
 import { Provider } from 'react-redux'
+
 import Routes from './routes';
 import * as reducers from './reducers';
 
 import "./assets/css/material-dashboard-react.css?v=1.5.0";
 
+// バージョンの原因でConnectedRouterでhistoryを設定するには
+// 「npm install --save react-router-redux@next」でインストールしてください。
+const history = createBrowserHistory();
+
 const store = createStore(
   combineReducers({
     ...reducers,
+    router: routerReducer,
   }),
   // applyMiddleware関数でredux-loggerを設定
   applyMiddleware(
     logger,
     thunk,
+    routerMiddleware(history)
   ),
 );
 
-ReactDOM.render((
-    <Provider store={store}>
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
       <Routes />
-    </Provider>
-  ), document.querySelector('#app')
+    </ConnectedRouter>
+  </Provider>
 );
+
+ReactDOM.render(<App/>, document.querySelector('#app'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Route, Switch } from 'react-router-dom';
 import { PrivateRoute } from '../components/privateRoute';
 import Login from '../containers/login';
 import Layout from '../containers/app';
-
-const history = createBrowserHistory();
 
 class Routes extends Component {
 
   render () {
     return (
-      <div>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <PrivateRoute path="/" component={Layout} />
-          </Switch>
-        </Router>
-      </div>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/" component={Layout} />
+      </Switch>
     );
   }
 }
